16e3bfa3caf77516447b8cb5960d719e3168f5a5
Vladimir Moskva <vladmos@yandex-team.ru>
2013-08-18 17:38:26 +0400

    Pretty console output

--- src/nirvana/commands.py
+++ src/nirvana/commands.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 import warnings
 import sys
 import re
@@ -9,6 +11,13 @@ import settings
 from utils import call_command
 
 
+def pretty_print(message):
+    """
+    Prints the message in bold cyan font to distinguish messages from Nirvana
+    """
+    print('\033[1;36m%s\033[1;m' % message)
+
+
 class Commands(object):
     @property
     def config(self):
@@ -18,12 +27,12 @@ class Commands(object):
                 try:
                     config = load_config()
                 except ConfigError, e:
-                    print('Nirvana configuration error: %s' % e)
+                    pretty_print('Nirvana configuration error: %s' % e)
                     sys.exit(1)
 
                 for warning in warnings_list:
                     if issubclass(warning.category, ConfigWarning):
-                        print('Nirvana configuration warning: %s' % warning.message)
+                        pretty_print('Nirvana configuration warning: %s' % warning.message)
 
                 self._config = config
 
@@ -49,13 +58,14 @@ class Commands(object):
         Create debianization for a project based on nirvana config files
         """
 
+        pretty_print('Preparing debianization...')
         Debianizer(self.config).execute(self.version)
-        print('The debianization is ready')
 
     def command_clean(self, args, remove_deb=True):
         """
         Remove debian/, setup.py, build-related files
         """
+        pretty_print('Cleaning...')
         remove_debianization()
         for package_config in self.config.packages:
             package_name = package_config['package']['name']
@@ -63,12 +73,11 @@ class Commands(object):
             if remove_deb:
                 remove_deb_package(package_name, self.version)
 
-        print('Clean completed')
-
     def command_changelog(self, args):
         """
         Create and update changelog
         """
+        pretty_print('Generating changelog...')
         create = args and args[0] == '--create'
 
         if create:
@@ -76,21 +85,26 @@ class Commands(object):
         else:
             call_command('dch -i --distributor=nirvana --changelog changelog')
 
-    def command_build(self, args):
+    def command_build(self, args, clean=True):
         """
         Make a deb-package
         """
         self.command_debianize(args)
+
+        pretty_print('Building a debian package...')
         call_command('debuild')
-        self.command_clean(args, remove_deb=False)
+
+        if clean:
+            self.command_clean(args, remove_deb=False)
 
     def command_install(self, args):
         """
         Install the package in the system
         """
-
         self.command_debianize(args)
-        call_command('debuild')
+        self.command_build(args, clean=False)
+
+        pretty_print('Installing the package...')
         for package_config in self.config.packages:
             call_command('sudo dpkg -i ../%s_%s_all.deb' % (
                 package_config['package']['name'],
